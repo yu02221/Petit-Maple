9dd6953fd69230ce9d029eff1ec31fb307c718da
yu02221 <63188840+yu02221@users.noreply.github.com>
2022-10-31 09:04:20 +0900

    First Commit
    메뉴 씬 UI, 마을 씬 NPC 애니메이션

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Animator BtnBoard;
+    public void OnClickedNewStartBtn()
+    {
+        StartCoroutine(BtnBoardDown());
+    }
+
+    IEnumerator BtnBoardDown()
+    {
+        BtnBoard.SetBool("goNextScene", true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("Village");
+    }
+}
