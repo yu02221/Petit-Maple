331889af8f12c1db2a68bb0f6d28c8f31e9b5279
yu02221 <63188840+yu02221@users.noreply.github.com>
2022-11-02 17:39:31 +0900

    2022-11-02
    엎드리기, 점프, 공격 모션 구현
    플랫폼 위 점프 미완

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,14 @@ public class PlayerController : MonoBehaviour
     public float gravity = -9.81f;
     public float jumpPower = 5.0f;
 
+    public LayerMask groundMask;
+
     private Vector3 velocity;
 
-    public bool onGround = true;
-    public bool isWalking = false;
+    private bool onGround = true;
+    private bool isWalking = false;
+    private bool isProstrated = false;
+    private bool isAttacking = false;
 
     public SpriteRenderer sr;
 
@@ -22,49 +26,47 @@ public class PlayerController : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
     }
 
-    private void Update()
-    {
-        
-        if (Input.GetKeyDown(KeyCode.LeftAlt) && onGround)
-        {
-            NormalJump();
-        }
-    }
-
     private void FixedUpdate()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical"));
 
         if (onGround)
-            MoveOnGround(input);
+        {
+            if (!isProstrated && !isAttacking)
+                Walking(input);
+
+            if (!isProstrated && !isAttacking
+                && Input.GetKey(KeyCode.LeftAlt))
+                NormalJump();
+
+            if (input.y < 0)
+            {
+                isProstrated = true;
+                velocity.x = 0;
+            }
+            else
+                isProstrated = false;
+
+            if (isAttacking)
+            {
+                velocity.x = 0;
+            }
+        }
         else
         {
-            velocity.y += gravity * Time.deltaTime;
+            velocity.y += gravity * Time.fixedDeltaTime;
         }
 
-        transform.Translate(velocity * Time.deltaTime);
+        if (!isAttacking && !isProstrated 
+            && Input.GetKey(KeyCode.LeftControl))
+        {
+            StartCoroutine(NormalAttack());
+        }
 
-    }
+        transform.Translate(velocity * Time.fixedDeltaTime);
 
-    private void MoveOnGround(Vector2 input)
-    {
-        velocity.x = input.x * moveSpeed;
-        velocity.y = 0;
-        isWalking = true;
-        if (velocity.x > 0)
-            sr.flipX = true;
-        else if (velocity.x < 0)
-            sr.flipX = false;
-        else
-            isWalking = false;
-
-        player.SetBool("isWalking", isWalking);
-    }
-
-    private void NormalJump()
-    {
-        onGround = false;
-        velocity.y = jumpPower;
+        SetAnimatorBool();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -73,7 +75,60 @@ public class PlayerController : MonoBehaviour
         {
             onGround = true;
         }
+        if (col.gameObject.CompareTag("Platform")
+            && velocity.y <= 1.0f)
+        {
+            onGround = true;
+        }
+    }
+    /*
+    private void CheckOnGround()
+    {
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundMask);
+        print(hit.distance);
+        if (hit.distance < 0.1f && velocity.y <= 2.0f)
+        {
+            onGround = true;
+            velocity.y = 0;
+        }
+        else
+        {
+            onGround = false;
+        }
+    }
+    */
+
+    private void Walking(Vector2 input)
+    {
+        velocity.x = input.x * moveSpeed;
+        isWalking = true;
+        if (velocity.x > 0)
+            sr.flipX = true;
+        else if (velocity.x < 0)
+            sr.flipX = false;
+        else
+            isWalking = false;
     }
 
+    private void NormalJump()
+    {
+        onGround = false;
+        velocity.y = jumpPower;
+    }
 
+    IEnumerator NormalAttack()
+    {
+        isAttacking = true;
+        player.SetTrigger("doAttack");
+        yield return new WaitForSeconds(1.0f);
+        isAttacking = false;
+    }
+
+    private void SetAnimatorBool()
+    {
+        player.SetBool("onGround", onGround);
+        player.SetBool("isWalking", isWalking);
+        player.SetBool("isProstrated", isProstrated);
+    }
 }
